7b08a6b39e6bef3dd89bad720acf22b80a868617
ysromantic <ysromantic@gmail.com>
2024-12-17 13:33:16 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-const program = new Command();
+import { program } from 'commander';
 
 program
-    .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
-    .helpOption('-h, --help', 'Output usage information');
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>');
 
 program.parse();
