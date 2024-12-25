7dd5891cda03277952834987b2bde6245b191a4b
ysromantic <ysromantic@gmail.com>
2024-12-25 15:37:07 +0300

    update gendiff.js, index.js; moved files: file1, file2 to __fixtures__; add asciinema to README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/ysromantic/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/ysromantic/frontend-project-46/actions)
+
+### Сравнение плоских файлов
+
+<a href="https://asciinema.org/a/74Hm7HV5QBftJXQLVag1SnOmS" target="_blank"><img src="https://asciinema.org/a/74Hm7HV5QBftJXQLVag1SnOmS.svg" /></a>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
 const genDiff = (filepath1, filepath2) => {
     const absolutePath1 = path.resolve(process.cwd(), filepath1);
     const absolutePath2 = path.resolve(process.cwd(), filepath2);
 
-    const file1 = fs.readFileSync(absolutePath1, 'utf-8');
-    const file2 = fs.readFileSync(absolutePath2, 'utf-8');
+    const file1Content = fs.readFileSync(absolutePath1, 'utf-8');
+    const file2Content = fs.readFileSync(absolutePath2, 'utf-8');
 
-    return `File1 content:\n${file1}\n\nFile2 content:\n${file2}`;
+    const data1 = JSON.parse(file1Content);
+    const data2 = JSON.parse(file2Content);
+
+    const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+
+    const result = keys.map((key) => {
+        if (!_.has(data2, key)) {
+            return `  - ${key}: ${data1[key]}`;
+        }
+        if (!_.has(data1, key)) {
+            return `  + ${key}: ${data2[key]}`;
+        }
+        if (!_.isEqual(data1[key], data2[key])) {
+            return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+        }
+        return `    ${key}: ${data1[key]}`;
+    });
+
+    return `{\n${result.join('\n')}\n}`;
 };
 
 export default genDiff;
