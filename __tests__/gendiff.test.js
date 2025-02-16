56c14352bbbe59098e19dbe2e5cb199f97bf4a52
ysromantic <ysromantic@gmail.com>
2025-02-16 19:40:10 +0300

    Fix CI and CodeClimate integration

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,13 +7,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const readJsonFixture = (filename) => JSON.parse(readFileSync(getFixturePath(filename), 'utf-8'));
 
 test('gendiff with flat JSON', () => {
     const file1 = getFixturePath('file1.json');
     const file2 = getFixturePath('file2.json');
-    const expected = readFixture('expected.txt');
+    const expected = readJsonFixture('expected.json');
 
-    const result = genDiff(file1, file2);
-    expect(result.trim()).toEqual(expected.trim());
+    const result = JSON.parse(genDiff(file1, file2));
+    expect(result).toEqual(expected);
 });
